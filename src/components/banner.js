c5e12838386e0870cd732cee465abab02ada144e
Emily Yu <emilyyu331@gmail.com>
2025-07-10 20:42:08 -0700

    feat: add falling heart animation with cleanup

--- src/components/banner.js
+++ src/components/banner.js
@@ -1,14 +1,47 @@
-import React from "react"
+import React, { useLayoutEffect } from "react"
 import { Link } from "gatsby"
 import pinkBrown from "../images/pinkBrown.jpeg"
+import heart from "../images/heart.png";
 
 const Banner = () => {
+
+    useLayoutEffect(() => {
+    const container = document.querySelector(".petal-animation");
+    const petals = [];
+
+    for (let i = 0; i < 15; i++) {
+      const petal = document.createElement("div");
+      petal.className = "petal";
+      const rotation = Math.floor(Math.random() * 360);
+      petal.style.setProperty('--rotate-end', `${rotation + 360}deg`);
+
+      // size 
+      const size = 30 + Math.random() * 30;
+      petal.style.width = `${size}px`;
+      petal.style.height = `${size}px`;
+
+      // position
+      petal.style.left = `${Math.random() * 100}vw`;
+      petal.style.animationDuration = `${10 + Math.random() * 10}s`;
+      petal.style.animationDelay = `${Math.random() * 8}s`;
+      
+      petal.style.backgroundImage = `url(${heart})`;
+      container.appendChild(petal);
+      petals.push(petal);
+    }
+
+    return () => {
+      petals.forEach((p) => p.remove());
+    };
+  }, []);
+
   return (
     <div className="banner" 
     style={{ 
       background: `url(${pinkBrown}) repeat`,
       }}
       >
+      <div className="petal-animation"></div>
       <div className="container">
         <div className="row">
           <div className="main-text">Hi! I'm Emily.</div>
